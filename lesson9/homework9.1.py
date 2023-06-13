06c7b36718c6fde59027cf7864b655f8831a0237
katsiaryna-lysenka <katya.lysenko.1999@gmail.com>
2023-06-13 16:09:17 +0300

    add file

---
+++ lesson9/homework9.1.py
@@ -0,0 +1,68 @@
+import self as self
+import time
+
+class Auto:
+
+    def __init__(self, brand, age, color='red', mark=0, weight=0):
+        print(brand, age, color, mark, weight)
+        self.brand = brand
+        self.age = age
+        self.color = color
+        self.mark = mark
+        self.weight = weight
+    def birthday(self):
+        example.age += 1
+        print(example.age)
+
+    def move(self):
+        print('move')
+
+    def stop(self):
+        print('stop')
+
+
+
+
+example = Auto('BMW', 54, 'black', 13, 76)
+Auto.move(self)
+Auto.stop(self)
+Auto.birthday(self)
+
+
+class Track(Auto):
+    def __init__(self, max_load, brand, age, color='green', mark=15, weight=99):
+        super().__init__(brand, age, color, mark, weight)
+        self.max_load = max_load
+
+
+    def move(self):
+        #super().move()
+        print('attention')
+        print('move')
+
+
+    def load(self):
+        time.sleep(1)
+        print('load')
+        time.sleep(1)
+
+class Car(Auto):
+    def __init__(self, max_speed, brand, age, color='black', mark=9, weight=150):
+        super().__init__(brand, age, color, mark, weight)
+        self.max_speed = max_speed
+
+    def move(self, max_speed):
+        print('move')
+        print(f'max speed is {max_speed}')
+
+
+Track(32, 'Alfa_Romeo', 4)
+Track.load(self)
+Track.move(self)
+Car.move(self, 150)
+Car(130, 'Bentley', 5, 'red', 8, 200)
+Car(170, 'Acura', 3)
+
+
+
+
